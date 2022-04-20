3f082966c3bc6e61ffc9c5c6726fd647af1a6147
MaheshWalke-Forcepoint <93905114+MaheshWalke-Forcepoint@users.noreply.github.com>
2022-04-20 13:35:36 +0530

    Update dynamodb.service.js

--- services/dynamodb.service.js
+++ services/dynamodb.service.js
@@ -2,8 +2,6 @@ const AWS = require('aws-sdk');
 const awsRegion = ("ap-south-1");
 
 const docClient = new AWS.DynamoDB.DocumentClient({
-    region: awsRegion,
-    // accessKeyId: "AKIAYNTWZK4G5D7WFB43",
-    // secretAccessKey:"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"
+    region: awsRegion
 });
 module.exports = docClient;
